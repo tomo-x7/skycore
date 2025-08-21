f4e36f74c25d508bebc52e9e53a6eb4b35a4bff2
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-08-21 12:28:06 +0900

    debug

--- units/YoutubeEmbed.tsx
+++ units/YoutubeEmbed.tsx
@@ -3,18 +3,20 @@ import { isExternalEmbed } from "./util";
 
 export default function YoutubeEmbed({ React, fetcher, post }: UnitArgs["embed"]): React.ReactNode {
 	try {
-		if (!isExternalEmbed(post.embed)) return null;
+		if (!isExternalEmbed(post.embed)) {console.log("not external embed");return null;}
 		const uri = new URL(post.embed.external.uri);
 		if (uri.host === "www.youtube.com" || uri.host === "youtube.com")
 			return <YoutubeEmbedInner React={React} v={uri.searchParams.get("v")} />;
 		if (uri.host === "youtu.be") return <YoutubeEmbedInner React={React} v={uri.pathname.slice(1)} />;
 		return null;
 	} catch (e) {
+		console.error("YoutubeEmbed error:", e);
 		return null;
 	}
 }
 
 function YoutubeEmbedInner({ v, React }: { v: string | null } & { React: UnitDefaultArgs["React"] }) {
+	console.log(`render youtube ${v}`)
 	if (v == null) return null;
 	return (
 		<iframe
