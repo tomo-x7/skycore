fcd9527d41add2f456cc4f51a9b5d8695222d70e
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-08-22 06:54:52 +0900

    スタイル改善

--- units/ExternalEmbed.tsx
+++ units/ExternalEmbed.tsx
@@ -1,25 +1,23 @@
-import type { UnitArgs } from "./type";
+import type { UnitArgs, UnitConfig } from "./type";
 import { isExternalEmbed } from "./util";
 
 export default function ExternalEmbed({ post, React }: UnitArgs["embed"]) {
 	if (!isExternalEmbed(post.embed)) return null;
 	return (
-		<div>
-			<a
-				href={post.embed.external.uri}
-				target="_blank"
-				rel="noopener noreferrer"
-				style={{ display: "block", paddingRight: 30 }}
-			>
-				{post.embed.external.thumb && (
-					<img
-						style={{ objectFit: "cover", aspectRatio: "1.91 / 1", width: "100%" }}
-						src={post.embed.external.thumb}
-					/>
-				)}
-				<div>{post.embed.external.title}</div>
-				<div>{post.embed.external.description}</div>
+		<div className="win-tomo-x-skycore-units-external-embed">
+			<a href={post.embed.external.uri} target="_blank" rel="noopener noreferrer">
+				{post.embed.external.thumb && <img src={post.embed.external.thumb} />}
+				<div className="content">
+					<div className="title">{post.embed.external.title}</div>
+					<div className="description">{post.embed.external.description}</div>
+					<hr />
+					<div className="hostname">{new URL(post.embed.external.uri).hostname}</div>
+				</div>
 			</a>
 		</div>
 	);
 }
+
+export const config: UnitConfig = {
+	css: ["ExternalEmbed.css"],
+};
