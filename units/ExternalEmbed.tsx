d188f0540c0337d25d2f2285107f24fc188edd2a
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-08-21 18:54:46 +0900

    externalのスタイル改善

--- units/ExternalEmbed.tsx
+++ units/ExternalEmbed.tsx
@@ -5,11 +5,21 @@ export default function ExternalEmbed({ post, React }: UnitArgs["embed"]) {
 	if (!isExternalEmbed(post.embed)) return null;
 	return (
 		<div>
-			{post.embed.external.thumb && (
-				<div style={{ aspectRatio: "1.91 / 1" }}>
-					<img style={{ objectFit: "cover" }} src={post.embed.external.thumb} />
-				</div>
-			)}
+			<a
+				href={post.embed.external.uri}
+				target="_blank"
+				rel="noopener noreferrer"
+				style={{ display: "block", paddingRight: 30 }}
+			>
+				{post.embed.external.thumb && (
+					<img
+						style={{ objectFit: "cover", aspectRatio: "1.91 / 1", width: "100%" }}
+						src={post.embed.external.thumb}
+					/>
+				)}
+				<div>{post.embed.external.title}</div>
+				<div>{post.embed.external.description}</div>
+			</a>
 		</div>
 	);
 }
