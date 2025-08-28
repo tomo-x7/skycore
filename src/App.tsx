321dce4aba2454af130062d1246faabd18db6cf6
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-08-29 04:48:12 +0900

    unitConfigへのリンクを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { MainLayout } from "./components/Layout";
 import type { Loader } from "./loader/types";
 import { Home } from "./pages/home";
 import { Providers } from "./providers";
+import { Settings } from "./pages/settings";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -15,7 +16,7 @@ const router = createBrowserRouter(
 				<Route element={<>feeds</>} path="/feeds" />
 				<Route element={<>lists</>} path="/lists" />
 				<Route element={<>user</>} path="/profile/:user" />
-				<Route element={<>settings</>} path="/settings" />
+				<Route element={<Settings />} path="/settings" />
 			</Route>
 			{/* フォールバック */}
 			<Route element={<>not found</>} path="*" />
---
+++ src/pages/settings.tsx
@@ -0,0 +1,8 @@
+export function Settings() {
+	return (
+		<div>
+			<h1>Settings</h1>
+			<a href="/unitConfig">unitconfig</a>
+		</div>
+	);
+}
