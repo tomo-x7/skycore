f260fb00444560c8c0d0638c2b552c31752bb9e1
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-09-04 03:44:53 +0900

    fix

--- units/TLPost.tsx
+++ units/TLPost.tsx
@@ -1,5 +1,5 @@
 import type { $Typed, AppBskyEmbedImages, AppBskyFeedDefs, AppBskyFeedPost } from "@atproto/api";
-import type { UnitArgs, UnitConfig } from "./type";
+import type { UnitArgs, UnitConfig, UnitDefaultArgs } from "./type";
 export default function TLPost({ post, isReply, hasReply, longReply, React, Embed }: UnitArgs["TLPost"]) {
 	return (
 		<div className="win-tomo-x-skycore-units-tlpost">
@@ -20,7 +20,7 @@ export default function TLPost({ post, isReply, hasReply, longReply, React, Embe
 					<div className="handle">@{post.author.handle ?? post.author.did}</div>
 				</div>
 				<div className="text">{(post.record as AppBskyFeedPost.Record).text}</div>
-				<Images post={post} />
+				<Images post={post} React={React} />
 				<Embed post={post} />
 
 				{longReply && <div className="openthread">スレッドをすべて表示</div>}
@@ -29,20 +29,22 @@ export default function TLPost({ post, isReply, hasReply, longReply, React, Embe
 	);
 }
 
-function Images({ post }: { post: AppBskyFeedDefs.PostView }) {
-	const embed=post.embed
-	if(!isImage(embed)) return null;
-	return <div className="images">
-		<img alt={embed.images[0]?.alt} src={embed.images[0]?.thumb} />
-		<img alt={embed.images[1]?.alt} src={embed.images[1]?.thumb} />
-		<img alt={embed.images[2]?.alt} src={embed.images[2]?.thumb} />
-		<img alt={embed.images[3]?.alt} src={embed.images[3]?.thumb} />
-	</div>;
+function Images({ post, React }: { post: AppBskyFeedDefs.PostView; React: UnitDefaultArgs["React"] }) {
+	const embed = post.embed;
+	if (!isImage(embed)) return null;
+	return (
+		<div className="images">
+			<img alt={embed.images[0]?.alt} src={embed.images[0]?.thumb} />
+			<img alt={embed.images[1]?.alt} src={embed.images[1]?.thumb} />
+			<img alt={embed.images[2]?.alt} src={embed.images[2]?.thumb} />
+			<img alt={embed.images[3]?.alt} src={embed.images[3]?.thumb} />
+		</div>
+	);
 }
 
-function isImage(embed: AppBskyFeedDefs.PostView["embed"]): embed is $Typed<AppBskyEmbedImages.View>  {
+function isImage(embed: AppBskyFeedDefs.PostView["embed"]): embed is $Typed<AppBskyEmbedImages.View> {
 	return embed?.$type === "app.bsky.embed.images#view";
-};
+}
 
 export const config: UnitConfig = {
 	css: ["./TLPost.css"],
